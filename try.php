3e311a2690c6e5681b541d5ca948498db604ee9c
teanaa <2017.tina.chandwani@ves.ac.in>
2020-05-07 13:43:05 +0530

    MyFirstWebsite

---
+++ try.php
@@ -0,0 +1,28 @@
+<?php
+include ('config.php');
+
+if (isset($_POST['name'])) {
+
+$name = "name";
+
+$query = "INSERT INTO pop ('id','name') VALUES ('','$name')";
+    $result = mysql_query($query,$cn);
+    if ($result) {
+    echo "<script type='text/javascript'>alert('submitted successfully!')</script>";
+}
+else
+{
+    echo "<script type='text/javascript'>alert('failed!')</script>";
+}
+}
+?>
+
+<html>
+<head>
+</head>
+<body>
+
+    <form action="" method="post">
+    Name:<input type="text" id="name" name="name"/>
+    <input type="submit" value="submit" name="submit" onclick="alert();"/>
+    </form>
